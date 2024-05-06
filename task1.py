3e6b36053edbc1a4fb44ee341f4796dfe67849d6
Tortisemilk420 <159003787+Tortisemilk420@users.noreply.github.com>
2024-05-06 12:26:25 -0700

    gfshghs

--- task1.py
+++ task1.py
@@ -1,6 +1,16 @@
 def A():
-
+ print("Hello")
 def B():
-
-
+ print("How are you")
 def C():
+  print("Hi")
+
+num = input("Enter one of the Capital letters A, B, or C: ")
+if num  == "A":
+    A()
+elif num == "B":
+    B()
+elif num == "C":
+    C()
+else:
+    print("Emter a valid letter")
--- task2.py
+++ task2.py
@@ -7,4 +7,22 @@ displays instructions and how to play
 
 game()
 plays the games
+
 """
+
+
+def title():
+    print("Guess a number between 1 and 100")
+
+def game():
+    guess = 0
+    import random
+    choice = random. randint(1, 100)
+    while choice != guess: 
+        guess = int(input("Pick a number between 1 and 100:"))
+    else:
+        print("You Guessed the corect number") 
+
+
+title()
+game()
